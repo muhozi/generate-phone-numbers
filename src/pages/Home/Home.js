9a306abae296e34f86a5f0ace513867a8b06f4db
Emery Muhozi <muhozie@gmail.com>
2019-05-20 12:44:42 +0300

    Set up style guide linting  - Set up folder structure  - Add git hooks

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './home.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Phone number generator</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
