3739acbce9602e8c390bc7a579a2f7db41264dd9
Emery Muhozi <muhozie@gmail.com>
2019-06-16 18:26:52 +0300

    Persist phone numbers in localStorage

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,35 +1,56 @@
 /* eslint-disable react/no-array-index-key */
-import React from 'react';
-import generateNumbers from '../../utils';
+import React, { Component } from 'react';
+import generateNumbers, { getPhoneNumbers } from '../../utils';
 import Layout from '../../components/Layout';
 import './home.scss';
 
-const Home = () => {
-  const phoneNumbers = generateNumbers();
-  return (
-    <Layout>
-      <div className="app-body">
-        <table className="phoneNumbers">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Phone number</th>
-              <th>Length</th>
-            </tr>
-          </thead>
-          <tbody>
-            {phoneNumbers.map((phone, key) => (
-              <tr key={key}>
-                <td>{key + 1}</td>
-                <td>{phone}</td>
-                <td>{phone.length}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </Layout>
-  );
-};
+export default class Home extends Component {
+  state = { phoneNumbers: [] };
 
-export default Home;
+  componentDidMount = () => {
+    this.getAllPhoneNumbers();
+  };
+
+  getAllPhoneNumbers = () => {
+    const phoneNumbers = getPhoneNumbers();
+    this.setState({ phoneNumbers });
+  };
+
+  generateNewPhoneNumbers = () => {
+    generateNumbers();
+    this.getAllPhoneNumbers();
+  };
+
+  render = () => {
+    const { phoneNumbers } = this.state;
+    return (
+      <Layout>
+        <div className="app-body">
+          <button type="button" onClick={this.generateNewPhoneNumbers}>
+            Generate
+          </button>
+          {phoneNumbers.length > 0 && (
+            <table className="phoneNumbers">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Phone number</th>
+                  <th>Length</th>
+                </tr>
+              </thead>
+              <tbody>
+                {phoneNumbers.map((phone, key) => (
+                  <tr key={key}>
+                    <td>{key + 1}</td>
+                    <td>{phone}</td>
+                    <td>{phone.length}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </div>
+      </Layout>
+    );
+  };
+}
--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,28 @@
+const storePhoneNumbers = phoneNumbers => {
+  if (localStorage.getItem('phoneNumbers') !== null) {
+    localStorage.removeItem('phoneNumbers');
+  }
+  localStorage.setItem('phoneNumbers', JSON.stringify(phoneNumbers));
+};
+
+export const getPhoneNumbers = () => {
+  try {
+    const allPhoneNumbers = localStorage.getItem('phoneNumbers');
+    if (allPhoneNumbers === null) {
+      return [];
+    }
+    return JSON.parse(allPhoneNumbers);
+  } catch (error) {
+    return [];
+  }
+};
 const generateNumbers = () => {
   const randNumbers = crypto.getRandomValues(new Uint32Array(10000));
   const phoneNumbers = [];
   randNumbers.forEach(phone => {
     phoneNumbers.push(phone.toString(10));
   });
+  storePhoneNumbers(phoneNumbers);
   return phoneNumbers;
 };
 
